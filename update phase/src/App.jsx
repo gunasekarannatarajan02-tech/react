d190f03731a10d08ccce683b2e71374708246136
guna <gunasekarannatarajan02@gmail.com>
2025-12-18 17:32:34 +0530

    up

--- update phase/src/App.jsx
+++ update phase/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Primitive from "./pages/Primitive";
-import NonPrimitive from "./pages/NonPrimitive";
+import Home from "./new/Home";
+import Primitive from "./new/Primitive";
+import NonPrimitive from "./new/NonPrimitive";
 
 function App() {
   return (
