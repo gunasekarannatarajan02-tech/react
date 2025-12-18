36fa2ff5882bedca0b4185e6662883dbf159f984
guna <gunasekarannatarajan02@gmail.com>
2025-12-18 17:33:03 +0530

    foldername changed

--- update phase/src/App.jsx
+++ update phase/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./new/Home";
-import Primitive from "./new/Primitive";
-import NonPrimitive from "./new/NonPrimitive";
+import Home from "./pages/Home";
+import Primitive from "./pages/Primitive";
+import NonPrimitive from "./pages/NonPrimitive";
 
 function App() {
   return (
